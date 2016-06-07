765d6829dd63ec88f1ae9782d1bbbc4a1784f187
toshieeeee <macbookairlove@gmail.com>
2016-06-07 18:43:59 +0900

    課題追加・ディレクトリ名変更

---
+++ homework_0605/06_array/challenge_associative_array.php
@@ -0,0 +1,7 @@
+<?php
+
+$fruit = array('name'=>'Lemon', 'price'=> 500, 'stock' => 3);
+
+echo $fruit['price'];
+
+?>
---
+++ homework_0605/07_superGlobalVal/ practice_global_send_elementary.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+
+<form method="post" action="practice_global_receive_elementary.php">
+
+  <p>
+    お名前 : <input id="my_name" type="text" name="my_name" value="">
+    <input type="submit" value="送信">
+  </p>
+
+</form>
+
+  
+</body>
+</html>
+
