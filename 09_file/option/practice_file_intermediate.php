8eea00d909e091a5e94a5d3584a04d4277024abf
toshieeeee <macbookairlove@gmail.com>
2016-06-24 22:15:27 +0900

    CSVデータ表示に成功！！

--- 09_file/option/practice_file_intermediate.php
+++ 09_file/option/practice_file_intermediate.php
@@ -18,7 +18,6 @@ $csv  = array();
 fwrite($temp, $data); //$tempファイルに、$dataの内容を書き込む。
 rewind($temp); //ファイルポインタを先頭の位置に戻す
 
-
 while (($data = fgetcsv($temp, 0, ",")) !== FALSE) {
 
 $data = implode(",", $data); //一行づつ、配列を","で連結して、文字列として返す
@@ -26,25 +25,78 @@ $csv[] = htmlentities($data);
 
 }
 
-
 $result = array(); //多次元配列を実現して、必要なデータだけを取り出せる形にする
+$table = '';
 
-for ($i=0; $i <10 ; $i++) { 
+for ($i = 0; $i < 30; $i++){ //表示件数を指定
 
   $result[$i] = explode(",", $csv[$i]); 
 
+  $table .= '<tr>';
+
+  $table .= '<td>' .$result[$i][2]. '</td>'; //郵便番号を格納
+
+  for ($k = 6; $k <= 8; $k++){
+
+    $village[$i][$k] =  $result[$i][$k]; //住所情報を格納
+
+    $table .= '<td>' .$village[$i][$k]. '</td>';
+
+  }
+
+  $table .= '</tr>' ;
+
 }
  
-var_dump($result);
-
-//多次元配列からデータを取り出す（ここができない）
-//$result[$i] [2 ~ 4] -こんなイメージ
-
-
 fclose($temp);
 
-
-
-
-
 ?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+
+<style type="text/css">
+  
+  .table {
+    border-collapse: collapse;
+  }
+  .table th {
+    background-color: #cccccc;
+  }
+
+  .table td {
+    padding: 4px;
+  }
+
+  .t-head td {
+    font-weight: bold;
+  }
+
+</style>
+</head>
+<body>
+
+  <p>以下にファイルから読み込んだ住所データを表示</p>
+
+  <p>住所データ（岩手県）</p>
+
+  <table class="table" border=1>
+    
+    <tr class="t-head">
+      <td>郵便番号</td>
+      <td>都道府県</td>
+      <td>市町村</td>
+      <td>町域</td>
+    </tr>
+
+    <?php echo $table ?>
+
+  </table>
+
+
+</body>
+</html>
